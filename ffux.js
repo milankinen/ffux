26cc407b4d0d954d943840ec323958c9a4fa232c
Matti Lankinen <m.lankinen@iki.fi>
2015-06-12 16:49:21 +0300

    Refactor API naming

--- ffux.js
+++ ffux.js
@@ -1,12 +1,41 @@
-const Bacon = require("baconjs")
 
 export default ffux
 
-ffux.run    = run
-ffux.pure   = pureAction
-ffux.impure = impureAction
+ffux.createStore = createStore
+ffux.pure        = pureAction
+ffux.impure      = impureAction
 
-function ffux(actions) {
+
+function ffux(stores) {
+  const state = zipObject(Object.keys(stores).map(name => {
+    const {initialState} = stores[name]
+    return [name, initialState]
+  }))
+
+  const actions = zipObject(Object.keys(stores).map(name => {
+    const {actionInterfaces} = stores[name]
+    return [name, actionInterfaces]
+  }))
+
+  const dispatchFn = (tickFn) => {
+    Object.keys(stores).forEach(name => {
+      const {stateChangeHandlers} = stores[name]
+      stateChangeHandlers.push((storeState) => {
+        state[name] = storeState
+        tickFn({state, actions})
+      })
+    })
+    tickFn({state, actions})
+  }
+
+  return {
+    dispatch: dispatchFn,
+    state: () => state,
+    actions: () => actions
+  }
+}
+
+function createStore(actions) {
   return function (initialState, deps) {
     const state = {value: initialState}
     const stateChangeHandlers = []
@@ -45,27 +74,6 @@ function ffux(actions) {
       stateChangeHandlers.forEach(h => h(newState))
     }
   }
-}
-
-function run(stores, onValueCallback) {
-  const state = zipObject(Object.keys(stores).map(name => {
-    const {initialState} = stores[name]
-    return [name, initialState]
-  }))
-
-  const actions = zipObject(Object.keys(stores).map(name => {
-    const {actionInterfaces} = stores[name]
-    return [name, actionInterfaces]
-  }))
-
-  Object.keys(stores).forEach(name => {
-    const {stateChangeHandlers} = stores[name]
-    stateChangeHandlers.push((storeState) => {
-      state[name] = storeState
-      onValueCallback({state, actions})
-    })
-  })
-  onValueCallback({state, actions})
 }
 
 function pureAction(fn) {
