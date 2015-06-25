e5bee593df34daff1b5522d984e1b496b2107f81
Matti Lankinen <m.lankinen@iki.fi>
2015-06-26 01:01:42 +0300

    Add comments to counter example

--- examples/counter.js
+++ examples/counter.js
@@ -4,35 +4,42 @@ const React = require("react"),
 const {createStore} = ffux
 
 const Counter = createStore({
-  actions: ['increment', 'decrement'],
-  state: ({counter}, {increment, decrement}) => {
-    return increment
-      .merge(decrement.map(d => -d))
+  actions: ['incrementN', 'decrementOne'],
+  state: (initialState, actionStreams) => {
+    const {counter} = initialState
+    const {incrementN, decrementOne} = actionStreams
+    // All Bacon.js tricks are permitted here!
+    return incrementN
+      .merge(decrementOne.map(-1))
       .scan(counter, (state, delta) => state + delta)
   }
 })
 
 const App = React.createClass({
   render() {
-    // passed model contains two properties:
+    // ffux model contains two properties:
     //   * "state" contains the current state of the application
     //   * "actions" contains the actions that can be performed
     const {counter} = this.props.state
 
     // actions are just functions that can be called with arguments normally
-    const {increment, decrement} = this.props.actions
+    const {incrementN, decrementOne} = this.props.actions
 
     return (
       <div>
         <div>Counter: {counter}</div>
-        <button onClick={() => increment(2)}>+2</button>
-        <button onClick={() => decrement(1)}>-</button>
+        <button onClick={() => incrementN(2)}>+2</button>
+        <button onClick={() => decrementOne()}>-</button>
       </div>
     )
   }
 })
 
-ffux({counter: Counter()}, {counter: 10})
-  .listen((model) => {
-    React.render(<App {...model} />, document.getElementById("app"))
-  })
+const initialState = {counter: 10}
+const stateModel   = {counter: Counter()}
+const dispatcher   = ffux(stateModel, initialState)
+
+// let's rock
+dispatcher.listen((model) => {
+  React.render(<App {...model} />, document.getElementById("app"))
+})
