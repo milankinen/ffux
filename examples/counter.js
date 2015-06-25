a723d480221b4afa72a21f20c5bb7962859b60f6
Matti Lankinen <m.lankinen@iki.fi>
2015-06-26 00:08:11 +0300

    Re-write counter example with event streams

--- examples/counter.js
+++ examples/counter.js
@@ -1,25 +1,14 @@
 const React = require("react"),
       ffux  = require("../ffux")
 
-const {createStore, pure, impure} = ffux
+const {createStore} = ffux
 
 const Counter = createStore({
-  actions: {
-    // pure actions are *synchronous* functions ({state}, ...action args) -> new state
-    // and the store's state can be modified ONLY by using these pure actions
-    increment: pure(({state}, step) => state + step),
-
-    decrement: pure(({state}, step) => state - step),
-
-    reset: pure(({state}) => 0),
-
-    // impure actions can't change state but they can call pure actions
-    // by using "self" reference. thus you can do sync/async/whatever
-    // you want in impure actions: these are the place to e.g. send data
-    // to the backend server
-    resetAsync: impure(({state, self}) => {
-      setTimeout(self.reset, 1000)
-    })
+  actions: ['increment', 'decrement'],
+  state: ({counter}, {increment, decrement}) => {
+    return increment
+      .merge(decrement.map(d => -d))
+      .scan(counter, (state, delta) => state + delta)
   }
 })
 
@@ -28,24 +17,22 @@ const App = React.createClass({
     // passed model contains two properties:
     //   * "state" contains the current state of the application
     //   * "actions" contains the actions that can be performed
-    // both "state" and "actions" contain the same keys that were
-    // used in "ffux(...)" construction
     const {counter} = this.props.state
 
     // actions are just functions that can be called with arguments normally
-    const {increment, decrement, resetAsync} = this.props.actions.counter
+    const {increment, decrement} = this.props.actions
 
     return (
       <div>
         <div>Counter: {counter}</div>
         <button onClick={() => increment(2)}>+2</button>
         <button onClick={() => decrement(1)}>-</button>
-        <button onClick={() => resetAsync()}>reset</button>
       </div>
     )
   }
 })
 
-ffux({counter: Counter(10)}).listen((model) => {
-  React.render(<App {...model} />, document.getElementById("app"))
-})
+ffux({counter: Counter()}, {counter: 10})
+  .listen((model) => {
+    React.render(<App {...model} />, document.getElementById("app"))
+  })
