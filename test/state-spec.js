fdc61c0c3c7e666b633d475ff0caa9c5d4d588de
Matti Lankinen <m.lankinen@iki.fi>
2015-06-27 23:55:20 +0300

    Add store state initialization tests

---
+++ test/state-spec.js
@@ -0,0 +1,37 @@
+const {expect} = require("chai"),
+      Bacon    = require("baconjs"),
+      ffux     = require("../lib/ffux-bacon")
+
+const {createStore} = ffux
+
+describe("state stream creation", () => {
+
+  const Counter = createStore({
+    state: (counter) => Bacon.constant(counter)
+  })
+
+  const Plus2 = createStore({
+    state: (value) => Bacon.constant(value + 2)
+  })
+
+  const NoProperty = createStore({
+    state: () => Bacon.never()
+  })
+
+
+  it("uses the given initial state to construct the store's state stream", done => {
+    ffux({a: Counter(2), b: Plus2(1)}).listen(({state}) => {
+      expect(state.a).to.equal(2)
+      expect(state.b).to.equal(3)
+      done()
+    })
+  })
+
+  it("throws an exception if store's state stream is not a Bacon.Property", done => {
+    try {
+      ffux({noProp: NoProperty()})
+    } catch (ignore) {
+      done()
+    }
+  })
+})
