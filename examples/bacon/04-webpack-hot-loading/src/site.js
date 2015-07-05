b2444c65e18d6445931d9e0d47535a29f6ca1e36
Matti Lankinen <m.lankinen@iki.fi>
2015-07-05 19:42:03 +0300

    Add Webpack hot loading Bacon.js example

---
+++ examples/bacon/04-webpack-hot-loading/src/site.js
@@ -0,0 +1,5 @@
+const React       = require("react"),
+      App         = require("./app")
+
+React.render(<App />, document.getElementById("app"))
+
---
+++ examples/bacon/04-webpack-hot-loading/src/todos.js
@@ -0,0 +1,30 @@
+const React = require("react")
+
+export default React.createClass({
+  render() {
+    const {newTodo, items, filter} = this.props.state
+    const {resetFilter, setNewTodoText, createItem} = this.props.actions
+    return (
+      <div>
+        <h1>TodoManager</h1>
+        <input placeholder="Filter items.."
+               value={filter}
+               onChange={e => resetFilter(e.target.value)}
+          />
+        <ul>{items.filter(it => it.display).map(it => <li>{it.text}</li>)}</ul>
+        <input placeholder="New item.."
+               value={newTodo}
+               onChange={e => setNewTodoText(e.target.value)}
+               onKeyDown={handleCreateItem}
+          />
+      </div>
+    )
+
+    function handleCreateItem(e) {
+      if (e.which === 13) {
+        createItem(newTodo)
+        setNewTodoText("")
+      }
+    }
+  }
+})
